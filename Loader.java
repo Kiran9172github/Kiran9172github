21528cf7da7e203f05a5c59d78aa810b9bd6cf6c
Kiran9172github <104568030+Kiran9172github@users.noreply.github.com>
2022-05-05 18:42:32 +0530

    Add files via upload

---
+++ Loader.java
@@ -0,0 +1,12 @@
+public class Loader	
+{
+public static void main(String[] args)
+ {
+ 	System.out.println(String.class.getClassLoader());
+ 	System.out.println(Loader.class.getClassLoader());
+	
+}
+
+
+
+}
